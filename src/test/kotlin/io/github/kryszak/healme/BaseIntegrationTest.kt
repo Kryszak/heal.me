3be3037bd32b5a8123f8eeba3db45ee68edc2438
Kryszak <prajskrzysztof@gmail.com>
2025-09-22 15:57:07 +0200

    add abstract modifier to it base test class

--- src/test/kotlin/io/github/kryszak/healme/BaseIntegrationTest.kt
+++ src/test/kotlin/io/github/kryszak/healme/BaseIntegrationTest.kt
@@ -9,13 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.jdbc.Sql
 import org.springframework.test.web.servlet.MockMvc
 
 @SpringBootTest
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
-class BaseIntegrationTest : ShouldSpec() {
+abstract class BaseIntegrationTest : ShouldSpec() {
 
     @Autowired
     lateinit var mockMvc: MockMvc
